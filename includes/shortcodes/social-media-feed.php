cba47c75fd288328bad01ce449a54f07ea9a099d
Phil Cable <pcable@wsu.edu>
2016-01-28 11:02:24 -0800

    Hight field fix

--- includes/shortcodes/social-media-feed.php
+++ includes/shortcodes/social-media-feed.php
@@ -56,11 +56,12 @@ class Item_County_Social_Media_Feed_PB extends Item_PB {
 		$defaults = array(
 			'source'        => '',
 			'url'           => '',
-			'height'        => '600',
+			'fb_height'     => '600',
 			'small_header'  => 'false',
 			'hide_cover'    => 'false',
 			'facepile'      => 'true',
 			'user'          => '',
+			't_height'      => '600',
 			'theme'         => '',
 			'replies'       => '',
 			'expand_photos' => '',
@@ -72,14 +73,15 @@ class Item_County_Social_Media_Feed_PB extends Item_PB {
 			return ( 'public' === $view ) ? '' : '<p>Click to configure feed</p>';
 		}
 
-		$height = ( is_numeric( $atts['height'] ) ) ? esc_html( $atts['height'] ) : 600;
+		$fb_height = ( is_numeric( $atts['fb_height'] ) ) ? esc_html( $atts['fb_height'] ) : 600;
+		$t_height = ( is_numeric( $atts['t_height'] ) ) ? esc_html( $atts['t_height'] ) : 600;
 
 		ob_start();
 		?>
-			<div class="county-responsive-media" style="padding-bottom:<?php echo $height; ?>px;">
+			<div class="county-responsive-media" style="padding-bottom:<?php echo $fb_height; ?>px;">
 
 				<?php if ( 'facebook' == $atts['source'] ) : ?>
-        <div class="fb-page" data-href="<?php echo esc_url( $atts['url'] ); ?>" data-tabs="timeline" data-width="500" data-adapt-container-width="true" data-height="<?php echo $height; ?>" data-small-header="<?php echo esc_attr( $atts['small_header'] ); ?>" data-hide-cover="<?php echo esc_attr( $atts['hide_cover'] ); ?>" data-show-facepile="<?php echo esc_attr( $atts['facepile'] ); ?>"></div>
+        <div class="fb-page" data-href="<?php echo esc_url( $atts['url'] ); ?>" data-tabs="timeline" data-width="500" data-adapt-container-width="true" data-height="<?php echo $fb_height; ?>" data-small-header="<?php echo esc_attr( $atts['small_header'] ); ?>" data-hide-cover="<?php echo esc_attr( $atts['hide_cover'] ); ?>" data-show-facepile="<?php echo esc_attr( $atts['facepile'] ); ?>"></div>
         	<?php if ( 'editor' === $view ) : ?>
         	<!--<script type="text/javascript" src="//connect.facebook.net/en_US/sdk.js?ver=0.22.3#xfbml=1&amp;version=v2.5"></script>--><!-- probably a terrible idea -->
           <?php endif; ?>
@@ -142,7 +144,7 @@ class Item_County_Social_Media_Feed_PB extends Item_PB {
 
 		$html  = Forms_PB::text_field( $base_name . '[url]', $atts['url'] , 'Facebook Page URL' , 'cpb-field-one-column' );
 		$html .= '<p style="font-size: 1.2rem; font-weight: bold; margin: 0 0 1rem;">Display Options</p>';
-		$html .= Forms_PB::text_field( $base_name . '[height]', $atts['height'] , 'Height');
+		$html .= Forms_PB::text_field( $base_name . '[fb_height]', $atts['fb_height'] , 'Height (in pixels)');
 		$html .= Forms_PB::checkbox_field( $base_name . '[small_header]', 'true', $atts['small_header'], 'Small Header' );
 		$html .= Forms_PB::checkbox_field( $base_name . '[hide_cover]', 'true', $atts['hide_cover'], 'Hide Cover Image' );
 		$html .= Forms_PB::checkbox_field( $base_name . '[facepile]', 'false', $atts['facepile'], 'Hide Friends' );
@@ -170,7 +172,7 @@ class Item_County_Social_Media_Feed_PB extends Item_PB {
 
 		$html  = Forms_PB::text_field( $base_name . '[user]', $atts['user'] , 'Twitter Username' , 'cpb-field-one-column' );
 		$html .= '<p style="font-size: 1.2rem; font-weight: bold; margin: 0 0 1rem;">Display Options</p>';
-		$html .= Forms_PB::text_field( $base_name . '[height]', $atts['height'] , 'Height');
+		$html .= Forms_PB::text_field( $base_name . '[t_height]', $atts['t_height'] , 'Height (in pixels)');
 		$html .= Forms_PB::select_field( $base_name . '[theme]', $atts['theme'], $twitter_themes, 'Theme' );
 		$html .= Forms_PB::checkbox_field( $base_name . '[replies]', 'true', $atts['replies'], "Show replies" );
 		$html .= Forms_PB::checkbox_field( $base_name . '[expand_photos]', 'false', $atts['expand_photos'], 'Auto expand photos' );
@@ -197,6 +199,7 @@ class Item_County_Social_Media_Feed_PB extends Item_PB {
 				if ( ! empty( $atts['url'] ) ) {
 					$clean['url'] = sanitize_text_field( $atts['url'] );
 				}
+				$clean['fb_height'] = ( ! empty( $atts['fb_height'] ) && is_numeric( $atts['fb_height'] ) ) ? (int) sanitize_text_field( $atts['fb_height'] ) : '600';
 				$clean['small_header'] = ( ! empty( $atts['small_header'] ) ) ? 'true' : '';
 				$clean['hide_cover']   = ( ! empty( $atts['hide_cover'] ) ) ? 'true' : '';
 				$clean['facepile']     = ( ! empty( $atts['facepile'] ) ) ? 'false' : '';
@@ -206,11 +209,12 @@ class Item_County_Social_Media_Feed_PB extends Item_PB {
 				if ( ! empty( $atts['user'] ) ) {
 					$clean['user'] = sanitize_text_field( $atts['user'] );
 				}
+				$clean['t_height'] = ( ! empty( $atts['t_height'] ) && is_numeric( $atts['t_height'] ) ) ? (int) sanitize_text_field( $atts['t_height'] ) : '600';
 				$clean['theme']         = ( ! empty( $atts['theme'] ) ) ? '' : ''; // find a default
 				$clean['replies']       = ( ! empty( $atts['replies'] ) ) ? 'true' : '';
 				$clean['expand_photos'] = ( ! empty( $atts['expand_photos'] ) ) ? 'false' : '';
 			}
-			$clean['height'] = ( ! empty( $atts['height'] ) && is_numeric( $atts['height'] ) ) ? (int) sanitize_text_field( $atts['height'] ) : '600';
+			
 		}
 
 		return $clean;
